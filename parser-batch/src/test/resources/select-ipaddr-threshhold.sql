bd08ca7fc13024668d1afcee7fec88421de1aae4
rodneyodvina <rodneyodvina@rodneys-mbp>
2018-04-08 01:25:19 -0400

    [parser-batch] UPdate sql

--- parser-batch/src/test/resources/select-ipaddr-threshhold.sql
+++ parser-batch/src/test/resources/select-ipaddr-threshhold.sql
@@ -4,3 +4,9 @@ SELECT IP_ADDR IP, count(IP_ADDR) CNT
 	where LOG_DT between '2017-01-01.15:00:00' and '2017-01-01.16:00:00'
 	group by IP
 	having count(IP_ADDR) > 200;
+
+select count(*) from WEB_LOG;
+
+delete from WEB_LOG;
+delete from WEB_LOG_HRLY_100;
+delete from WEB_LOG_DLY_250;
